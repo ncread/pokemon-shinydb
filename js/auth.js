81e3f9f51c174d01cca38e3d1ad41dee04f17666
ncread <noahcread@gmail.com>
2026-06-04 15:09:12 -0700

    fix more pathing issues

--- js/auth.js
+++ js/auth.js
@@ -61,7 +61,7 @@ async function getProfile(userId) {
 async function requireAuth() {
     const user = await getUser();
     if (!user) {
-        window.location.href = '../';
+        window.location.href = '/pokemon-shinydb/';
         return null;
     }
     return user;
@@ -70,5 +70,5 @@ async function requireAuth() {
 // Redirect to dashboard if already logged in (for the login page).
 async function redirectIfAuth() {
     const user = await getUser();
-    if (user) window.location.href = '../dashboard/';
+    if (user) window.location.href = '/pokemon-shinydb/';
 }
